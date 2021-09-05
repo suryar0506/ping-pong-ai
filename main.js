a04e81e3c1b83121572aeb72ad8724e3ceb640b0
suryar0506 <72110042+suryar0506@users.noreply.github.com>
2021-09-04 22:21:03 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,21 @@
 
+function setup(){
+  canvas = createCanvas(700,600);
+  canvas.parent("canvas");
+  video = createCapture(VIDEO);
+  video.hide();
+
+  poseNet = ml5.poseNet(video, modelLoaded);
+}
+
+
+function draw(){
+  image(video, 0, 0, 700, 600);
+}
+
+function modelLoaded(){
+  console.log("Model is loaded.");
+}
 /*created by prashant shukla */
 /* Need to delete this
 var paddle2 =10,paddle1=10;
